6eb3c66eb625e3b0eaa8b4eee967e4f69ff08d5d
NickDrew <nicholasadrew@hotmail.com>
2023-05-28 16:21:25 +0100

    sweak scorboard

--- src/components/Scoreboard/Scoreboard.tsx
+++ src/components/Scoreboard/Scoreboard.tsx
@@ -3,16 +3,14 @@ import styles from "./scoreboardStyle.module.css"
 
 export interface IScoreboardProps {
     score: number;
-    level: number;
 }
 
 export const Scoreboard: React.FC<IScoreboardProps> = (props) => {
-    const { score, level } = props;
+    const { score } = props;
     return <div className={styles.scoreboardOuter}>
         <div className={styles.scoreboardInner}>
             <div>
                 <div>Score:{score}</div>
-                <div>Level:{level}</div>
             </div>
         </div>
     </div>
