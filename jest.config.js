d2304c6e50b201818cbf5954f7a81a54976215a0
NickDrew <nicholasadrew@hotmail.com>
2023-05-29 13:41:38 +0100

    setup test coverage

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,21 @@ module.exports = {
   testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
     "\\.(css|scss)$": "identity-obj-proxy",
-  }
+  },
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "index.ts",
+    ".d.ts",
+    ".module.css",
+    "<rootDir>/src/App.tsx",
+    ".test.ts"
+  ],
+  coverageThreshold: {
+    "global": {
+      "branches": 90,
+      "functions": 90,
+      "lines": 90,
+      "statements": 90
+    }
+  },
 };
